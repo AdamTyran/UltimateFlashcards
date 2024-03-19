aee4e973c0878c21ec7fc51c9d996d897911a28d
Adam <adam.tyran@gmail.com>
2024-03-19 20:25:26 +0100

    Added working getData function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,45 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
+import knex from "knex";
 
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.BACKEND_PORT;
+
+app.use(express.json());
+
+const connectToDatabase = knex({
+  client: "pg",
+  connection: {
+    host: process.env.PG_ADDRESS,
+    port: 5433,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+  },
+});
+
+// const result = connectToDatabase("users")
+//   .insert({
+//     username: "abcde",
+//     email: "abcde@gmail.com",
+//     password: "123",
+//   })
+//   .then((abc) => {
+//     console.log(abc);
+//   });
+
+const getData = async () => {
+  try {
+    const result = await connectToDatabase.select().from("users");
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getData();
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
