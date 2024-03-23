f787c504dd969737c9152f31bdaea1aad154de4f
Adam <adam.tyran@gmail.com>
2024-03-23 16:27:58 +0100

    Added words router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
 import { WordsController } from "./controllers/words/words.controller";
+import cors from "cors";
+import router from "./routes/words/words.route";
 
 dotenv.config();
 
@@ -8,19 +10,10 @@ const app: Application = express();
 const PORT = process.env.BACKEND_PORT;
 
 app.use(express.json());
+app.use(cors());
+app.use("/", router);
 
-// const getData = async () => {
-//   try {
-//     const result = await postgres.select("*").from("users");
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// getData();
-
-app.get("/", WordsController.getWords);
+// app.get("/", WordsController.getWords);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
