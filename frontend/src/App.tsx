76ea9f3494c8070b5497d771a5a021b83cd15709
Adam <adam.tyran@gmail.com>
2024-03-24 14:22:18 +0100

    initialise frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-gradient-to-tr from-green-700 via-green-800 to-green-900">
+      <div className="h-14 bg-gradient-to-tr from-violet-700 via-violet-800 to-violet-950"></div>
+    </div>
+  );
+}
+
+export default App;
