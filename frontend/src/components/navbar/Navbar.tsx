de12887ab8c44227241c628983ef5fd0d48dfe92
Adam <adam.tyran@gmail.com>
2024-03-28 17:41:29 +0100

    Added navbar buttons

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Brightness3OutlinedIcon from "@mui/icons-material/Brightness3Outlined";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import NavbarButtons from "../../utils/navbar-buttons";
+import { Link } from "react-router-dom";
 
 interface Props {
   darkMode: boolean;
@@ -9,14 +11,28 @@ interface Props {
 
 const Navbar = ({ darkMode, toggleDarkMode }: Props) => {
   return (
-    <div className="flex items-center h-14 bg-neutral-300 dark:bg-gray-800">
+    <nav className="flex items-center justify-between h-14 bg-neutral-300 dark:bg-gray-800 shadow">
       <button
-        className="ml-2 h-10 w-10 rounded-full bg-blue-300"
+        className="ml-2 h-10 w-10 rounded-full bg-gray-600  dark:bg-neutral-300"
         onClick={toggleDarkMode}
       >
         {darkMode ? <LightModeOutlinedIcon /> : <Brightness3OutlinedIcon />}
       </button>
-    </div>
+      <div className="flex mx-auto text-center gap-2">
+        {NavbarButtons.map((item) => (
+          <div key={item.id}>
+            {/* <Link */}
+            <div
+              // to={item.path}
+              className="h-10 bg-neutral-400 rounded-md border-solid text-white hover:bg-neutral-500 p-2 font-bold"
+            >
+              {item.name}
+              {/* </Link> */}
+            </div>
+          </div>
+        ))}
+      </div>
+    </nav>
   );
 };
 
---
+++ frontend/src/utils/navbar-buttons.ts
@@ -0,0 +1,14 @@
+const NavbarButtons = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "Word Sets",
+    path: "/sets",
+  },
+];
+
+export default NavbarButtons;
