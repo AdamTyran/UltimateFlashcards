77ad3cf554bf09825ade77328ca36beef5e5b831
Adam <adam.tyran@gmail.com>
2024-04-07 19:36:19 +0200

    Added text to home page

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Text from "../../components/text/Text";
 
 const Home = () => {
+  const displayedText = "What do you want to learn today?";
+
   return (
-    <div className="flex h-1/2 justify-center items-center">
-      <h1 className="text-8xl font-bold">What do you want to learn today?</h1>
+    <div className="flex h-1/2 justify-center items-center text-6xl font-bold">
+      <Text displayedText={displayedText} />
     </div>
   );
 };
